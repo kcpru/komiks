31dbe13ab058ac659bcbc0b26540f63de776365a
kcpru <47855645+kcpru@users.noreply.github.com>
2020-04-20 21:58:59 +0000

    Deploying to gh-pages from  @ 7a5638e63460b07171476a3e5956614ce4a4b977 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/komiks/precache-manifest.0a95632ab341591d760f74f1aeb24908.js"
+  "/precache-manifest.253b0c6d95f569208c59147dfdc9a24a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/komiks/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
