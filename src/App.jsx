3ed12fffa35b3356502340dbd5cbf8cc60d3ea04
Kacper Slusarczyk <kacper12345678909@gmail.com>
2020-04-20 23:25:51 +0200

    Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const GlobalStyle = createGlobalStyle`
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router basename={'/komiks'}>
         <GlobalStyle />
         <div>
           <Navigation />
